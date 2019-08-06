ac60fdf3c50b636948519064967540601ef8675b
shortland <ilankleiman@gmail.com>
2019-08-06 11:50:24 -0400

    remove async & more info in readme

--- userscript.js
+++ userscript.js
@@ -1,21 +1,19 @@
 // ==UserScript==
 // @name         No Democracy? No Paywall.
 // @namespace    https://github.com/shortland/NoDemocracyNoPaywall
-// @version      0.1
+// @version      0.1.1
 // @description  Title.
 // @author       shortland
 // @match        https://www.washingtonpost.com/*
 // @grant        none
 // ==/UserScript==
 
-(async() => {
-    console.log("No Democracy? No Paywall.");
-    (doIt = function() {
-        window.TWP = null;
-        setTimeout(doIt, 10);
-    })();
+console.log("No Democracy? No Paywall.");
+(doIt = function() {
+    window.TWP = null;
+    setTimeout(doIt, 10);
 })();
 
 setTimeout(() => {
     document.getElementsByTagName("html")[0].style.overflow = "scroll";
-}, 1000);
+}, 1500);
